34c1e1009d1d0e41ae8a10eb8bbed86d7a374bed
chris984 <chrstnmadrid@gmail.com>
2024-05-02 22:38:09 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+    
+    
+     private static int tile[] = new int[4 * 4];
+    
+    public static void main(String[] args)
+    {
+        
+//    
+////       int res = 16;
+////       
+////       int result = res % tile.length;
+////       
+////       System.out.print(result);
+//        
+//       
+       
+    
+    }
+    
+}
